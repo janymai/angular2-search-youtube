508a0042cf31d36526d8878be079241062f7c5da
My Mai <my.mai@asnet.com.vn>
2016-04-08 16:22:03 +0700

    search-youtube component

--- src/app/components/search-youtube/search-youtube.ts
+++ src/app/components/search-youtube/search-youtube.ts
@@ -1,23 +1,157 @@
-import {Component} from 'angular2/core';
+import {
+  Component,
+  Injectable,
+  bind,
+  OnInit,
+  ElementRef,
+  EventEmitter,
+  Inject
+} from 'angular2/core';
 import {FORM_DIRECTIVES} from 'angular2/common';
+import {
+  Http,
+  Response
+} from 'angular2/http';
+import {Observable} from 'rxjs';
 
+export var YOUTUBE_API_KEY: string = 'AIzaSyDOfT_BO81aEZScosfTYMruJobmpjqNeEk';
+export var YOUTUBE_API_URL: string = 'https://www.googleapis.com/youtube/v3/search';
+
+// object will hold the data we want from each result
+class SearchResult {
+  id: string;
+  title: string;
+  description: string;
+  thumbnailUrl: string;
+  videoUrl: string;
+
+  constructor(obj?: any) {
+    this.id = obj && obj.id || null;
+    this.title = obj && obj.title || null;
+    this.description = obj && obj.description || null;
+    this.thumbnailUrl = obj && obj.thumbnailUrl || null;
+    this.videoUrl = obj && obj.videoUrl || 'https://www.youtube.com/watch?v=${this.id}';
+  }
+}
+
+// YouTubeService
+@Injectable()
+export class YouTubeService {
+  constructor(public http: Http,
+    @Inject(YOUTUBE_API_KEY) private apiKey: string,
+    @Inject(YOUTUBE_API_URL) private apiUrl: string) {
+
+  }
+
+  search(query: string): Observable<SearchResult[]> {
+    let params: string = [
+      `q=${query}`,
+      `key=${this.apiKey}`,
+      `part=snippet`,
+      `type=video`,
+      `maxResults=10`
+    ].join('&');
+    let queryUrl: string = `${this.apiUrl}?${params}`;
+    return this.http.get(queryUrl)
+      .map((response: Response) => {
+        return (<any>response.json()).items.map(item => {
+          return new SearchResult({
+            id: item.id.videoId,
+            title: item.snippet.title,
+            description: item.snippet.description,
+            thumbnailUrl: item.snippet.thumbnails.high.url
+          });
+        });
+      });
+  }
+}
+
+export var youTubeServiceInjectables: Array<any> = [
+  bind(YouTubeService).toClass(YouTubeService),
+  bind(YOUTUBE_API_KEY).toValue(YOUTUBE_API_KEY),
+  bind(YOUTUBE_API_URL).toValue(YOUTUBE_API_URL)
+];
+
+@Component({
+  outputs: ['loading', 'results'],
+  selector: 'search-box',
+  template: `
+    <input type="text" class="form-control" placeholder="Search" autofocus>
+  `
+})
+
+class SearchBox implements OnInit {
+  loading: EventEmitter<boolean> = new EventEmitter<boolean>();
+  results: EventEmitter<SearchResult[]> = new EventEmitter<SearchResult[]>();
+
+  constructor(public youtube: YouTubeService,
+              private el: ElementRef) {
+  }
+
+  ngOnInit(): void {
+    // convert the `keyup` event into an observable stream
+    Observable.fromEvent(this.el.nativeElement, 'keyup')
+      .map((e: any) => e.target.value)
+      .filter((text: string) => text.length > 1)
+      .debounceTime(250)
+      .do(() => this.loading.next(true))
+      .map((query: string) => this.youtube.search(query))
+      .switch()
+      .subscribe(
+        (results: SearchResult[]) => {
+          this.loading.next(false);
+          this.results.next(results);
+        },
+        (err: any) => {
+          console.log(err);
+          this.loading.next(false);
+        },
+        () => {
+          this.loading.next(false);
+        }
+      );
+  }
+
+}
+
+@Component({
+  inputs: ['result'],
+  selector: 'search-result',
+  template: `
+   <div class="col-sm-6 col-md-3">
+      <div class="thumbnail">
+        <img src="{{result.thumbnailUrl}}">
+        <div class="caption">
+          <h3>{{result.title}}</h3>
+          <p>{{result.description}}</p>
+          <p><a href="{{result.videoUrl}}"
+                class="btn btn-default" role="button">Watch</a></p>
+        </div>
+      </div>
+    </div>
+  `
+})
+
+export class SearchResultComponent {
+  result: SearchResult;
+}
 
 @Component({
   selector: 'search-youtube',
-  directives: [...FORM_DIRECTIVES],
+  directives: [SearchBox, SearchResultComponent, ...FORM_DIRECTIVES],
   pipes: [],
   template: require('./search-youtube.html'),
   styles: [require('./search-youtube.scss')]
 })
 
 export class SearchYoutube {
+  results: SearchResult[];
 
-  constructor() {
-    // Do stuff
+  updateResults(results: SearchResult[]): void {
+    this.results = results;
   }
 
   ngOnInit() {
-    console.log('Hello Home');
+    console.log('Hello SearchYoutube');
   }
-
 }
